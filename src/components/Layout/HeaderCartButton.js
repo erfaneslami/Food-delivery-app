e9094b7076f6f09bf0bb4f90aea3f219d0c58c3e
erfan eslami <erfan.88.eslami@gmail.com>
2022-04-02 23:18:40 +0430

    fix mobile view style

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -29,7 +29,7 @@ const HeaderCartButton = (props) => {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span className={classes.cartText}>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
